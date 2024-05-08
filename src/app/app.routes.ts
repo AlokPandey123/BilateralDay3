14a7f1bf27bb80c594e09cf0e7c296b0344f2649
KumarVK <128793825+AlokPandey123@users.noreply.github.com>
2024-05-08 18:08:28 +0530

    final push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { BlogComponent } from './component/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", component:HomeComponent},
+    {path:"about", component:AboutUsComponent},
+    {path:"contact", component:ContactUsComponent},
+    {path:"blog", component:BlogComponent}
+];
